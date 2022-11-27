63bfebfafcbbab94e462ac208dec5357641c6018
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2022-11-27 13:13:54 +0100

    Update XLSX_replace_values_via_mapping.py

--- XLSX_replace_values_via_mapping.py
+++ XLSX_replace_values_via_mapping.py
@@ -1,23 +1,47 @@
 # Script for replacing cell values in XSLX based on mapping
 # get new words from CSV file and replace old words
-# written by Monika Barget in November 2022
+# written for the DigiKAR project by Monika Barget in November 2022
 
 import xlsxwriter
 import csv
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
+import pdftotext
 import os
+from collections import defaultdict
 
-CSV_FILE='C:\\Users\\###\\event_mapping.csv' # sample file containing original keywords and mapping to final words
+# define file containing original terms and ontological mapping
 
-with open(CSV_FILE, encoding="utf-8", errors="ignore") as f:
-    data = pd.read_csv(f, sep=";")
-    events_old=data['event_name'].values
-    events_new=data['event_type'].values
-    func_new=data['pers_function'].values
+event_ontology='C:\\Users\\mobarget\\Documents\\Seafile\\DigiKAR_DATEN\\Python\\InputLists\\event_ontology.csv' 
+title_ontology='C:\\Users\\mobarget\\Documents\\Seafile\\DigiKAR_DATEN\\Python\\InputLists\\title_ontology.csv' 
+function_ontology='C:\\Users\\mobarget\\Documents\\Seafile\\DigiKAR_DATEN\\Python\\InputLists\\function_ontology.csv'
+place_ontology='C:\\Users\\mobarget\\Documents\\Seafile\\DigiKAR_DATEN\\Python\\InputLists\\place_ontology.csv' 
+
+# open ontology files
+
+with open(event_ontology, encoding="utf-8", errors="ignore") as f_event:
+    data_e = pd.read_csv(f_event, sep=";")
+    events_old=data_e['event_old'].values
+    events_new=data_e['event_type'].values
+    func_new=data_e['pers_function'].values
     
-# open input files as dataframe and replace or add values
+#with open(title_ontology, encoding="utf-8", errors="ignore") as f_title:
+    #data_t = pd.read_csv(f_title, sep=";")
+    #title_old=data_t['title_old'].values
+    #events_new=data_t['per_title'].values
+
+#with open(function_ontology, encoding="utf-8", errors="ignore") as f_function:
+    #data_f = pd.read_csv(f_function, sep=";")
+    #function_old=data_f['function_old'].values
+    #function_new=data_f['pers_function'].values
+    
+with open(place_ontology, encoding="utf-8", errors="ignore") as f_place:
+    data_p = pd.read_csv(f_place, sep=";")
+    places_old=data_p['place_old'].values
+    places_new=data_p['place_new'].values
+    
+# function to process data
 
 def extract_information(filenames):
         
@@ -33,29 +57,90 @@ def extract_information(filenames):
 
     f = pd.concat(frame_list, axis=0, ignore_index=False, sort=False)
         
-# replace event types in EVENT column
+# replace words in EVENT column & check if corresponding function needs to be updated
             
     for e_old in events_old:
         print(type(e_old))
         try:
-            e_new=data.loc[data['event_name'] == e_old, 'event_type'].values[0]
+            e_new=data_e.loc[data_e['event_old'] == e_old, 'event_type'].values[0]
             print(e_new)
             f['event_name'] = f['event_name'].replace(e_old, e_new)
+            
+# check if event results in a specific function and add it if necessary
+
+            f_rel=data_e.loc[data_e['title_old'] == e_old, 'pers_function'].values[0]
+            
+            if f_rel==True:
+                f['title_old'] = f['title_old'].replace(e_old, e_new)
+            else:
+                print("No function found.")
+                continue
             
         except KeyError:
             print("Key Error")
             continue
-                    
+            
+# find "hidden" places rows and add values to PLACE column
+            
+    for p in places_new:
+        print(p)
+        try:
+            p_add=f[f["place_new"].map(lambda place_new: p in place_new) & f["inst_name"].map(lambda inst_name: p in inst_name)]
+            print(p_add)
+            
+# Still raises ValueError: Columns must be same length as key
+# Code will be fixed ASAP
+
+            f['place_name'] =(f['place_name'].map(str) + "/" + p_add)
+            
+        except KeyError:
+            print("Key Error")
+            continue
+            
+        print(f)
+                
 # write all results to new EXCEL file
 
-    workbook='C:\\Users\\###\\Profs_mapped.xlsx'
-    writer = pd.ExcelWriter(workbook, engine='xlsxwriter') # create a Pandas Excel writer using XlsxWriter as engine
-    f.to_excel(writer, sheet_name='Mapped') # convert the dataframe to an XlsxWriter Excel object
-    writer.save() # close and save the Excel file
+    workbook='C:\\Users\\mobarget\\Documents\\Seafile\\DigiKAR_DATEN\\Python\\Results\\ProfEvents_FINAL\\Profs_mapped.xlsx'
+    writer = pd.ExcelWriter(workbook, engine='xlsxwriter') # create a Pandas Excel writer using XlsxWriter as the engine.
+    f.to_excel(writer, sheet_name='Mapped2') # Convert the dataframe to an XlsxWriter Excel object.
+    writer.save() # Close the Pandas Excel writer and output the Excel file.
                    
 # iterate through all XLSX files in directoy        
 
 if __name__ == '__main__':
-    filenames = "C:\\Users\\###\\ProfEvents_MAPPING"
+    filenames = "C:\\Users\\mobarget\\Documents\\Seafile\\DigiKAR_DATEN\\Python\\Results\\ProfEvents_MAPPING"
     extract_information(filenames)
     print("Done.") 
+    
+# ADDITIONAL OPTIONS to be added when all ontology lists are in place:
+
+'''            
+# replace words in TITLE column
+            
+    for t_old in title_old:
+        print(t_old)
+        try:
+            t_new=data.loc[data['title_old'] == t_old, 'pers_title'].values[0]
+            print(e_new)
+            f['title_old'] = f['title_old'].replace(t_old, t_new)                
+            
+        except KeyError:
+            print("Key Error")
+            continue
+            
+# replace words in FUNCTION column
+            
+    for f_old in function_old:
+        print(f_old)
+        try:
+            f_new=data_f.loc[data_f['event_name'] == f_old, 'event_type'].values[0]
+            print(f_new)
+            f['event_name'] = f['event_name'].replace(f_old, f_new)
+            
+        except KeyError:
+            print("Key Error")
+            continue
+            
+        print(f)
+'''
